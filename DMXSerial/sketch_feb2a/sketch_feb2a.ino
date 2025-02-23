7df461a02de1171c03f094b5215df49355bf91a2
Alex Borst <alexborst@gmail.com>
2025-02-22 22:45:55 -0500

    Folder restructure

--- DMXSerial/sketch_feb2a/sketch_feb2a.ino
+++ DMXSerial/sketch_feb2a/sketch_feb2a.ino
@@ -1,27 +1,29 @@
-#include <DMXSerial.h>
+//#include <DMXSerial.h>
 #include <SoftwareSerial.h>
 
 SoftwareSerial mySerial(10, 11); // RX, TX on digital pins 10 and 11
 
 void setup() {
-    mySerial.begin(9600); // Use SoftwareSerial for debugging
-    DMXSerial.init(DMXReceiver); // Initialize DMX receiver mode
-    mySerial.println("DMX Receiver Ready");
+  //Serial.begin(9600);
+  mySerial.begin(9600); // Use SoftwareSerial for debugging
+  //DMXSerial.init(DMXReceiver); // Initialize DMX receiver mode
+  mySerial.println("DMX Receiver Ready");
 }
 
 void loop() {
-    static uint32_t lastPrintTime = 0;
-    uint32_t currentTime = millis();
+  mySerial.print("Beginning READ for DMX Data... ");
+  //   static uint32_t lastPrintTime = 0;
+  //   uint32_t currentTime = millis();
 
-    if (currentTime - lastPrintTime >= 1000) {
-        mySerial.print("DMX Data: ");
+  //   if (currentTime - lastPrintTime >= 1000) {
+  //       mySerial.print("DMX Data: ");
         
-        for (int i = 1; i <= 10; i++) {
-            mySerial.print(DMXSerial.read(i));
-            mySerial.print(" ");
-        }
+  //       for (int i = 1; i <= 10; i++) {
+  //           mySerial.print(DMXSerial.read(i));
+  //           mySerial.print(" ");
+  //       }
 
-        mySerial.println();
-        lastPrintTime = currentTime;
-    }
+  //       mySerial.println();
+  //       lastPrintTime = currentTime;
+  //   }
 }
